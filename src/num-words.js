e309f75d48866cc5ab56309726e95f9c6d050ea8
John Whittenburg <jawhittenburg@gmail.com>
2016-03-16 23:17:48 -0500

    starting index.js

--- src/num-words.js
+++ src/num-words.js
@@ -1,1 +1,6 @@
 'use strict';
+
+function numWords(stringOfWords) {
+  const words = stringOfWords.split(` `);
+  return words.length;
+}
--- src/read-time.js
+++ src/read-time.js
@@ -1,1 +1,6 @@
 'use strict';
+
+function readTime(stringOfWords, wpm = 1) {
+  const math = Math.ceil(numWords(stringOfWords) / wpm);
+  return math;
+}
